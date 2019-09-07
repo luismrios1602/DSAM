18740148668e4cebf4812c8940b71d94bd641b1f
luismrios1602 <54143281+luismrios1602@users.noreply.github.com>
2019-09-07 14:39:36 -0500

    Add files via upload

---
+++ TallerCondicionales-puntoNET/Ejercicio4/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: Lenovo
+ * Fecha: 7/09/2019
+ * Hora: 12:10 p. m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace Ejercicio4
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+		double carga=0, unidad, total;
+		string nombre;
+
+        Console.WriteLine("ELECTRICARIBE CAREVERGA S.A.R.\n---------------------------------------\n"+
+        "Ingrese los siguientes datos: \n*Nombre \n*Unidades Consumidas \n");
+		Console.Write("Ingrese su nombre: ");
+        nombre = Console.ReadLine();
+		Console.Write("Ingrese las unidades consumidas: ");
+		unidad = double.Parse(Console.ReadLine());
+
+        if (unidad <= 199) {
+            carga = 1.20;
+        } else if (unidad >= 200 && unidad < 400) {
+            carga = 1.50;
+        } else if (unidad >= 400 && unidad < 600) {
+            carga = 1.80;
+        } else if (unidad >= 600) {
+            carga = 2.00;
+        }
+
+        total = carga * unidad;
+        Console.WriteLine("\nELECTRICARIBE CAREVERGA S.A.R.\n---------------------------------------\n"+
+        "Señor "+nombre+", el total a pagar es de: $"+total);
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("\nPress any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
---
+++ TallerCondicionales-puntoNET/Ejercicio6/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: Lenovo
+ * Fecha: 7/09/2019
+ * Hora: 12:56 p. m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace Ejercicio6
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			double n,f;
+			
+			Console.WriteLine("Ingrese el valor de N");
+			n = double.Parse(Console.ReadLine());
+			
+			double variable1 = 1/Math.Sqrt(5);
+			double variable2 = (1+Math.Sqrt(5))/2;
+			double variable3 = (1-Math.Sqrt(5))/2;
+			f = n/(variable1*(Math.Pow(variable2,n)-Math.Pow(variable3,n)));
+			
+	       	Console.WriteLine("F(N)= "+f);
+				
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
