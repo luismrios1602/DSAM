a50ad2ec1b02593913b91b5f937d947de45e7629
Luis Miguel Ríos Pérez <54143281+luismrios1602@users.noreply.github.com>
2019-10-23 08:38:25 -0500

    Funciones - Funcion Pow Manual

---
+++ Funciones/FuncionPow/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: CBN
+ * Fecha: 23/10/2019
+ * Hora: 08:09 a.m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace FuncionPow
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			int basesinha, exponente;
+			Console.Write("Ingrese la base vale mondá esa: ");
+			basesinha = int.Parse(Console.ReadLine());
+			Console.Write("Ingrese el exponente que vale más mondá todavía: ");
+			exponente = int.Parse(Console.ReadLine());
+			
+			Console.WriteLine(basesinha+"^"+exponente+" = "+Powiar(basesinha, exponente));
+			Console.WriteLine(basesinha+"^"+exponente+" = "+Math.Pow(basesinha, exponente));
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+		
+		public static int Powiar(int basesinha, int exponente){
+			int potencia=1;
+			
+			for (int i = 1; i <= exponente; i++) {
+				potencia *= basesinha;
+			}
+			return potencia;
+		}
+	}
+}
---
+++ Funciones/OperacionesBasicas/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperacionesBasicas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            Console.Write("Ingrese el primer número: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese el segundo número: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(num1+" + "+num2+" = "+Sumar(num1, num2));
+            Console.WriteLine(num1+" - "+num2+" = "+Restar(num1, num2));
+            Console.WriteLine(num1+" x "+num2+" = "+Multiplicar(num1, num2));
+            Console.WriteLine(num1+" / "+num2+" = "+Dividir(num1, num2));
+
+            Console.WriteLine("\nPresione cualquier tecla para continuar...");
+            Console.ReadKey();
+        }
+
+        public static int Sumar(int num1, int num2)
+        {
+            int resultado = num1 + num2;
+            return resultado;
+        }
+
+        public static int Restar(int num1, int num2)
+        {
+            int resultado = num1 - num2;
+            return resultado;
+        }
+
+        public static int Multiplicar(int num1, int num2)
+        {
+            int resultado = num1 * num2;
+            return resultado;
+        }
+
+        public static float Dividir(int num1, int num2)
+        {
+            float resultado = num1 / num2;
+            return resultado;
+            
+        }
+    }
+}
