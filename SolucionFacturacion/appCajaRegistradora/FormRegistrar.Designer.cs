b093fc41b3ceaf21eea0bf0b14950c0b69df8f36
Luis Miguel Ríos Pérez <54143281+luismrios1602@users.noreply.github.com>
2020-02-18 21:24:38 -0500

    Permite registrar un producto

--- SolucionFacturacion/appCajaRegistradora/FormRegistrar.Designer.cs
+++ SolucionFacturacion/appCajaRegistradora/FormRegistrar.Designer.cs
@@ -28,23 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormRegistrar));
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.lblCodigo = new System.Windows.Forms.Label();
-            this.nudCodigo = new System.Windows.Forms.NumericUpDown();
-            this.lblNombre = new System.Windows.Forms.Label();
-            this.txtNombre = new System.Windows.Forms.TextBox();
-            this.lblSelecUni = new System.Windows.Forms.Label();
-            this.cbUnidad = new System.Windows.Forms.ComboBox();
-            this.lblPrecio = new System.Windows.Forms.Label();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.btnRegistrar = new System.Windows.Forms.Button();
+            this.nudPrecio = new System.Windows.Forms.NumericUpDown();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.cbUnidad = new System.Windows.Forms.ComboBox();
+            this.lblSelecUni = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.nudCodigo = new System.Windows.Forms.NumericUpDown();
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.errorForm = new System.Windows.Forms.ErrorProvider(this.components);
             this.panel1.SuspendLayout();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPrecio)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.nudCodigo)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorForm)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -55,7 +58,7 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(465, 47);
+            this.panel1.Size = new System.Drawing.Size(507, 47);
             this.panel1.TabIndex = 0;
             // 
             // label1
@@ -72,7 +75,7 @@
             // 
             this.groupBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.groupBox1.Controls.Add(this.btnRegistrar);
-            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.nudPrecio);
             this.groupBox1.Controls.Add(this.lblPrecio);
             this.groupBox1.Controls.Add(this.cbUnidad);
             this.groupBox1.Controls.Add(this.lblSelecUni);
@@ -82,72 +85,47 @@
             this.groupBox1.Controls.Add(this.lblCodigo);
             this.groupBox1.Location = new System.Drawing.Point(12, 53);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(441, 175);
+            this.groupBox1.Size = new System.Drawing.Size(483, 175);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "DATOS GENERALES";
             // 
-            // lblCodigo
+            // btnRegistrar
             // 
-            this.lblCodigo.AutoSize = true;
-            this.lblCodigo.Location = new System.Drawing.Point(7, 20);
-            this.lblCodigo.Name = "lblCodigo";
-            this.lblCodigo.Size = new System.Drawing.Size(88, 13);
-            this.lblCodigo.TabIndex = 0;
-            this.lblCodigo.Text = "Ingrese el código";
-            this.lblCodigo.Click += new System.EventHandler(this.lblCodigo_Click);
+            this.btnRegistrar.Location = new System.Drawing.Point(357, 92);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(120, 23);
+            this.btnRegistrar.TabIndex = 8;
+            this.btnRegistrar.Text = "Registrar Producto";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
             // 
-            // nudCodigo
+            // nudPrecio
             // 
-            this.nudCodigo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.nudCodigo.Location = new System.Drawing.Point(7, 37);
-            this.nudCodigo.Maximum = new decimal(new int[] {
+            this.nudPrecio.DecimalPlaces = 2;
+            this.nudPrecio.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.nudPrecio.Location = new System.Drawing.Point(156, 92);
+            this.nudPrecio.Maximum = new decimal(new int[] {
             1410065407,
             2,
             0,
             0});
-            this.nudCodigo.Minimum = new decimal(new int[] {
-            1,
-            0,
-            0,
-            0});
-            this.nudCodigo.Name = "nudCodigo";
-            this.nudCodigo.Size = new System.Drawing.Size(120, 22);
-            this.nudCodigo.TabIndex = 1;
-            this.nudCodigo.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
-            this.nudCodigo.UpDownAlign = System.Windows.Forms.LeftRightAlignment.Left;
-            this.nudCodigo.Value = new decimal(new int[] {
-            1,
-            0,
-            0,
-            0});
+            this.nudPrecio.Name = "nudPrecio";
+            this.nudPrecio.Size = new System.Drawing.Size(147, 21);
+            this.nudPrecio.TabIndex = 7;
+            this.nudPrecio.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.nudPrecio.ThousandsSeparator = true;
+            this.nudPrecio.UpDownAlign = System.Windows.Forms.LeftRightAlignment.Left;
+            this.nudPrecio.KeyDown += new System.Windows.Forms.KeyEventHandler(this.nudPrecio_KeyDown);
             // 
-            // lblNombre
+            // lblPrecio
             // 
-            this.lblNombre.AutoSize = true;
-            this.lblNombre.Location = new System.Drawing.Point(150, 20);
-            this.lblNombre.Name = "lblNombre";
-            this.lblNombre.Size = new System.Drawing.Size(153, 13);
-            this.lblNombre.TabIndex = 2;
-            this.lblNombre.Text = "Ingrese el nombre del producto";
-            this.lblNombre.Click += new System.EventHandler(this.lblNombre_Click);
-            // 
-            // txtNombre
-            // 
-            this.txtNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.txtNombre.Location = new System.Drawing.Point(153, 37);
-            this.txtNombre.Name = "txtNombre";
-            this.txtNombre.Size = new System.Drawing.Size(282, 23);
-            this.txtNombre.TabIndex = 3;
-            // 
-            // lblSelecUni
-            // 
-            this.lblSelecUni.AutoSize = true;
-            this.lblSelecUni.Location = new System.Drawing.Point(7, 76);
-            this.lblSelecUni.Name = "lblSelecUni";
-            this.lblSelecUni.Size = new System.Drawing.Size(106, 13);
-            this.lblSelecUni.TabIndex = 4;
-            this.lblSelecUni.Text = "Seleccione la unidad";
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Location = new System.Drawing.Point(153, 76);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(85, 13);
+            this.lblPrecio.TabIndex = 6;
+            this.lblPrecio.Text = "Ingrese el precio";
             // 
             // cbUnidad
             // 
@@ -163,49 +141,73 @@
             this.cbUnidad.Name = "cbUnidad";
             this.cbUnidad.Size = new System.Drawing.Size(121, 21);
             this.cbUnidad.TabIndex = 5;
+            this.cbUnidad.SelectedIndexChanged += new System.EventHandler(this.cbUnidad_SelectedIndexChanged);
             this.cbUnidad.SelectedValueChanged += new System.EventHandler(this.cbUnidad_SelectedValueChanged);
+            this.cbUnidad.KeyDown += new System.Windows.Forms.KeyEventHandler(this.cbUnidad_KeyDown);
             // 
-            // lblPrecio
+            // lblSelecUni
             // 
-            this.lblPrecio.AutoSize = true;
-            this.lblPrecio.Location = new System.Drawing.Point(153, 76);
-            this.lblPrecio.Name = "lblPrecio";
-            this.lblPrecio.Size = new System.Drawing.Size(85, 13);
-            this.lblPrecio.TabIndex = 6;
-            this.lblPrecio.Text = "Ingrese el precio";
+            this.lblSelecUni.AutoSize = true;
+            this.lblSelecUni.Location = new System.Drawing.Point(7, 76);
+            this.lblSelecUni.Name = "lblSelecUni";
+            this.lblSelecUni.Size = new System.Drawing.Size(106, 13);
+            this.lblSelecUni.TabIndex = 4;
+            this.lblSelecUni.Text = "Seleccione la unidad";
             // 
-            // numericUpDown1
+            // txtNombre
             // 
-            this.numericUpDown1.DecimalPlaces = 2;
-            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.numericUpDown1.Location = new System.Drawing.Point(156, 92);
-            this.numericUpDown1.Maximum = new decimal(new int[] {
+            this.txtNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.txtNombre.Location = new System.Drawing.Point(153, 37);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(282, 23);
+            this.txtNombre.TabIndex = 3;
+            this.txtNombre.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtNombre_KeyDown);
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(150, 20);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(153, 13);
+            this.lblNombre.TabIndex = 2;
+            this.lblNombre.Text = "Ingrese el nombre del producto";
+            this.lblNombre.Click += new System.EventHandler(this.lblNombre_Click);
+            // 
+            // nudCodigo
+            // 
+            this.nudCodigo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nudCodigo.Location = new System.Drawing.Point(7, 37);
+            this.nudCodigo.Maximum = new decimal(new int[] {
             1410065407,
             2,
             0,
             0});
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(147, 23);
-            this.numericUpDown1.TabIndex = 7;
-            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
-            this.numericUpDown1.ThousandsSeparator = true;
-            this.numericUpDown1.UpDownAlign = System.Windows.Forms.LeftRightAlignment.Left;
+            this.nudCodigo.Name = "nudCodigo";
+            this.nudCodigo.Size = new System.Drawing.Size(120, 22);
+            this.nudCodigo.TabIndex = 1;
+            this.nudCodigo.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.nudCodigo.UpDownAlign = System.Windows.Forms.LeftRightAlignment.Left;
+            this.nudCodigo.KeyDown += new System.Windows.Forms.KeyEventHandler(this.nudCodigo_KeyDown);
             // 
-            // btnRegistrar
+            // lblCodigo
             // 
-            this.btnRegistrar.Location = new System.Drawing.Point(315, 92);
-            this.btnRegistrar.Name = "btnRegistrar";
-            this.btnRegistrar.Size = new System.Drawing.Size(120, 23);
-            this.btnRegistrar.TabIndex = 8;
-            this.btnRegistrar.Text = "Registrar Producto";
-            this.btnRegistrar.UseVisualStyleBackColor = true;
-            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Location = new System.Drawing.Point(7, 20);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(88, 13);
+            this.lblCodigo.TabIndex = 0;
+            this.lblCodigo.Text = "Ingrese el código";
+            this.lblCodigo.Click += new System.EventHandler(this.lblCodigo_Click);
+            // 
+            // errorForm
+            // 
+            this.errorForm.ContainerControl = this;
             // 
             // FormRegistrar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(465, 240);
+            this.ClientSize = new System.Drawing.Size(507, 240);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.panel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -217,8 +219,9 @@
             this.panel1.ResumeLayout(false);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPrecio)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.nudCodigo)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorForm)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -234,8 +237,9 @@
         private System.Windows.Forms.Label lblNombre;
         private System.Windows.Forms.ComboBox cbUnidad;
         private System.Windows.Forms.Label lblSelecUni;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown nudPrecio;
         private System.Windows.Forms.Label lblPrecio;
         private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.ErrorProvider errorForm;
     }
 }
--- SolucionFacturacion/appCajaRegistradora/FormRegistrar.cs
+++ SolucionFacturacion/appCajaRegistradora/FormRegistrar.cs
@@ -31,14 +31,115 @@ namespace appCajaRegistradora
 
         private void cbUnidad_SelectedValueChanged(object sender, EventArgs e)
         {
-            int pos = cbUnidad.SelectedIndex;
-            var valor = cbUnidad.SelectedItem.ToString();
-            MessageBox.Show("Pos: "+pos+" -> "+valor);
+            errorForm.Clear();
         }
 
         private void btnRegistrar_Click(object sender, EventArgs e)
         {
+            if (validarFormulario())
+            {
+                var resp = MessageBox.Show("¿Seguro que quieres almacenar el producto "+Convert.ToInt64(nudCodigo.Value)+" : "+txtNombre.Text+"?","Almacenar",MessageBoxButtons.YesNo,MessageBoxIcon.Question);
+                if (resp == DialogResult.Yes)
+                {
+                    Productos p = new Productos(Convert.ToInt32(nudCodigo.Value),txtNombre.Text,cbUnidad.SelectedItem.ToString(),Convert.ToDouble(nudPrecio.Value));
+                    if (bd.addProducto(p))
+                    {
+                        MessageBox.Show("Guardado!");
+                        nudCodigo.Value = 0;
+                        txtNombre.Clear();
+                        cbUnidad.SelectedIndex = -1;
+                        nudPrecio.Value = 0;
+                        nudCodigo.Focus();
+                    }
+                } 
+            }
+            else
+            {
+                MessageBox.Show("Valiendo verga");
+            }
+        }
 
+        private bool validarFormulario()
+        {
+
+            bool respuesta = true;
+
+            if (Convert.ToInt64(nudCodigo.Value) == 0)
+            {
+                errorForm.SetError(nudCodigo, "El codigo no puede ser cero (0)");
+                respuesta = false;
+            } else
+            {
+                if (string.IsNullOrEmpty(txtNombre.Text))
+                {
+                    errorForm.Clear();
+                    errorForm.SetError(txtNombre, "Valor vacío o nulo");
+                    respuesta = false;
+                }
+                else
+                {
+                    if (cbUnidad.SelectedItem==null)
+                    {
+                        errorForm.Clear();
+                        errorForm.SetError(cbUnidad, "Seleccione una Unidad");
+                        respuesta = false;
+                    }
+                    else
+                    {
+                        if (Convert.ToInt64(nudPrecio.Value) <= 0)
+                        {
+                            errorForm.SetError(nudPrecio, "El precio no puede ser cero (0)");
+                            respuesta = false;
+                        }
+                        else
+                        {
+                            errorForm.Clear(); //Si todo está correcto que elimine los errores generados
+                        }
+                    }
+                }
+            }
+
+            return respuesta;
+        }
+
+        private void nudCodigo_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                txtNombre.Focus();
+            }
+        }
+
+        private void txtNombre_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                cbUnidad.Focus();
+                cbUnidad.DroppedDown = true;
+            }
+        }
+
+        private void cbUnidad_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                nudPrecio.Focus();
+                
+            }
+        }
+
+        private void cbUnidad_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void nudPrecio_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnRegistrar.Focus();
+
+            }
         }
     }
 }
